1dfe301c2e28a44b3ce983122b42b00a9f5eccbe
Fery Ardian Syah <feryardsh123@gmail.com>
2022-12-07 23:33:55 +0700

    fix add review

--- src/controllers/courseReviews.js
+++ src/controllers/courseReviews.js
@@ -2,107 +2,123 @@ import Course from "../models/Course.js";
 import asyncHandler from "express-async-handler";
 import Instructor from "../models/Instructor.js";
 import {
-	findCourseInPurchasedCourses,
-	findReviewedCourses,
+    findCourseInPurchasedCourses,
+    findReviewedCourses,
 } from "../utils/helperFunctions.js";
 
 //@ desc Review a course
 //@ route /course/id/reviews
 //@ access Private
 const addReview = asyncHandler(async (req, res) => {
-	const { reviewText, rating } = req.body;
+    const { reviewText, rating } = req.body;
 
-	const course = await Course.findById(req.params.id);
-	if (!course) {
-		res.status(404);
-		throw new Error("No Course");
-	}
+    const course = await Course.findById(req.params.id);
+    if (!course) {
+        res.status(404);
+        throw new Error("No Course");
+    }
 
-	const purchasedCourse = findCourseInPurchasedCourses(
-		req.student,
-		req.params.id,
-	);
-	if (!purchasedCourse) {
-		res.status(404);
-		throw new Error("Buy the course to review it.");
-	}
+    const purchasedCourse = findCourseInPurchasedCourses(
+        req.student,
+        req.params.id
+    );
 
-	const reviewed = findReviewedCourses(course, req.student._id);
+    if (!purchasedCourse) {
+        res.status(404);
+        throw new Error("Buy the course to review it.");
+    }
 
-	if (reviewed) {
-		res.status(404);
-		throw new Error("Cannot review more than once");
-	}
+    const reviewed = findReviewedCourses(course, req.student._id);
 
-	const review = {
-		reviewText,
-		rating,
-		user: req.student._id,
-	};
+    if (reviewed) {
+        res.status(404);
+        throw new Error("Cannot review more than once");
+    }
 
-	const instructor = await Instructor.findById(course.createdBy);
+    const review = {
+        reviewText,
+        rating,
+        user: req.student._id,
+    };
 
-	if (!instructor) {
-		res.status(404);
-		throw new Error("Instructor profile not found");
-	}
+    const instructor = await Instructor.findById(course.createdBy);
 
-	course.courseReviews.push(review);
+    if (!instructor) {
+        res.status(404);
+        throw new Error("Instructor profile not found");
+    }
 
-	course.avgRating =
-		course.courseReviews.reduce((acc, review) => {
-			return review.rating + acc;
-		}, 0) / course.courseReviews.length;
+    course.courseReviews.push(review);
 
-	course.numOfRatings++;
-	instructor.numberOfReviews += 1;
+    course.avgRating =
+        course.courseReviews.reduce((acc, review) => {
+            return review.rating + acc;
+        }, 0) / course.courseReviews.length;
 
-	await course.save();
-	await instructor.save();
-	res.status(201).json({ message: "Course Reviewed" });
+    course.numOfRatings++;
+    instructor.numberOfReviews += 1;
+
+    await course.save();
+    await instructor.save();
+
+    const getTotalRating = await Instructor.findById(course.createdBy).populate(
+        "courses",
+        "avgRating"
+    );
+    const divide = getTotalRating.courses.filter((rating) => {
+        return rating.avgRating >= 1;
+    });
+
+    getTotalRating.avgRating =
+        getTotalRating.courses.reduce((acc, review) => {
+            return review.avgRating + acc;
+        }, 0) / divide.length;
+
+    await getTotalRating.save();
+    res.status(201).json({ message: "Course Reviewed" });
 });
 
 //@ desc Remove a Review
 //@ route /course/id/reviews
 //@ access Private
 const deleteReview = asyncHandler(async (req, res) => {
-	const purchasedCourse = findCourseInPurchasedCourses(
-		req.student,
-		req.params.id,
-	);
+    const purchasedCourse = findCourseInPurchasedCourses(
+        req.student,
+        req.params.id
+    );
 
-	if (!purchasedCourse) {
-		res.status(404);
-		throw new Error("Buy the course to review it.");
-	}
+    if (!purchasedCourse) {
+        res.status(404);
+        throw new Error("Buy the course to review it.");
+    }
 
-	const course = await Course.findByIdAndUpdate(
-		req.params.id,
-		{
-			$pull: { courseReviews: { user: req.student._id } },
-		},
-		{ new: true },
-	);
+    const course = await Course.findByIdAndUpdate(
+        req.params.id,
+        {
+            $pull: { courseReviews: { user: req.student._id } },
+        },
+        { new: true }
+    );
 
-	const reviewed = findReviewedCourses(course, req.student._id);
+    const reviewed = findReviewedCourses(course, req.student._id);
 
-	if (!reviewed) {
-		res.status(404);
-		throw new Error("Review not found");
-	}
+    if (!reviewed) {
+        res.status(404);
+        throw new Error("Review not found");
+    }
 
-	await course.save();
-	res.status(200).json({ message: "Deleted Successfully" });
+    await course.save();
+    res.status(200).json({ message: "Deleted Successfully" });
 });
 
 //@ desc Remove all reviews
 //@ route /course/reviews
 //@ access public for now
 const deleteAllReviews = asyncHandler(async (req, res) => {
-	const course = await Course.findById(req.params.id);
+    const course = await Course.findById(req.params.id);
 
-	course.courseReviews = [];
-	await course.save();
+    course.courseReviews = [];
+    await course.save();
 });
 
 export { addReview, deleteReview, deleteAllReviews };
