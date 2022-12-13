15ed09cc676529d29298ad465a6bf647148fbf00
Fery Ardian Syah <feryardsh123@gmail.com>
2022-12-14 00:46:21 +0700

    fix: auth user

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,237 +1,238 @@
 import Student from "../models/Student.js";
 import asyncHandler from "express-async-handler";
 import Instructor from "../models/Instructor.js";
-import cloudinary from "../config/cloudinary.js"
+import cloudinary from "../config/cloudinary.js";
 
 // @desc Get all students
 // @route /
 // @access Public for now
 const getAllStudentProfile = asyncHandler(async (req, res) => {
-	const students = await Student.find({});
-	
-	if (students.length === 0) {
-		res.status(404);
-		throw new Error("No Students found");
-	}
-	res.json({ students });
+    const students = await Student.find({});
+
+    if (students.length === 0) {
+        res.status(404);
+        throw new Error("No Students found");
+    }
+    res.json({ students });
 });
 
 // @desc Register Student
 // @route /signup
 // @access Public
 const createStudent = asyncHandler(async (req, res) => {
-	const { name, email, password, headline, isInstructor, profession, aboutMe } =
-		req.body;
+    const {
+        name,
+        email,
+        password,
+        headline,
+        isInstructor,
+        profession,
+        aboutMe,
+    } = req.body;
 
-	if (!name && !email && !password) {
-		res.status(404);
-		throw new Error("Fields is required");
-	}
+    if (!name && !email && !password) {
+        res.status(404);
+        throw new Error("Fields is required");
+    }
 
-	const isStudent = await Student.findOne({ email });
+    const isStudent = await Student.findOne({ email });
 
-	if (isStudent) {
-		res.status(404);
-		throw new Error("Email already exists.");
-	}
+    if (isStudent) {
+        res.status(404);
+        throw new Error("Email already exists.");
+    }
 
-	const student = new Student({
-		name,
-		email,
-		password,
-		isInstructor,
-		headline: headline ? headline : "",
-	});
+    const student = new Student({
+        name,
+        email,
+        password,
+        isInstructor,
+        headline: headline ? headline : "",
+    });
 
-	if (isInstructor) {
-		const instructor = new Instructor({
-			name: student.name,
-			studentID: student._id,
-			profession,
-			aboutMe,
-		});
+    if (isInstructor) {
+        const instructor = new Instructor({
+            name: student.name,
+            studentID: student._id,
+            profession,
+            aboutMe,
+        });
 
-		await instructor.save();
-	}
+        await instructor.save();
+    }
 
-	const token = await student.generateAuthToken();
+    const token = await student.generateAuthToken();
 
-	res.status(201).json({ name, email, headline, token });
+    res.status(201).json({ name, email, headline, token });
 });
 
 // @desc Register Instructor
 // @route /signup
 // @access Public
 const createInstructor = asyncHandler(async (req, res) => {
-	const { profession, aboutMe } = req.body;
-	const studentId = req.student._id
+    const { profession, aboutMe } = req.body;
+    const studentId = req.student._id;
 
-	if (!profession && !aboutMe) {
-		res.status(404);
-		throw new Error("Fields required");
-	}
+    if (!profession && !aboutMe) {
+        res.status(404);
+        throw new Error("Fields required");
+    }
 
+    const isStudent = await Student.findByIdAndUpdate(studentId, {
+        isInstructor: true,
+    });
+    if (!isStudent) {
+        res.status(404);
+        throw new Error("Can't update data");
+    }
 
-	const isStudent = await Student.findByIdAndUpdate(studentId, {
-		isInstructor: true
-	});
-	if (!isStudent) {
-		res.status(404);
-		throw new Error("Can't update data");
-	}
-	
-	try {
-		const instructorData = new Instructor({
-			studentID: studentId,
-			profession,
-			aboutMe,
-		})
-		await instructorData.save()
-	} catch (error) {
-		return res.status(201).json(error);
-	}
+    try {
+        const instructorData = new Instructor({
+            studentID: studentId,
+            profession,
+            aboutMe,
+        });
+        await instructorData.save();
+    } catch (error) {
+        return res.status(201).json(error);
+    }
 
-	res.status(201).json({message: "Success add data"});
+    res.status(201).json({ message: "Success add data" });
 });
-
 
 // @desc Login Student
 // @route /login
 // @access Public
 const loginStudent = asyncHandler(async (req, res) => {
-	const { email, password } = req.body;
+    const { email, password } = req.body;
 
-	const student = await Student.findStudentByCredentials(email, password);
+    const student = await Student.findStudentByCredentials(email, password);
 
-	if (!student) {
-		res.status(404);
-		throw new Error("Please enter correct email and password.");
-	}
-	
-	const token = await student.generateAuthToken();
+    if (!student) {
+        res.status(404);
+        throw new Error("Please enter correct email and password.");
+    }
 
-	res.status(200).json({
-		name: student.name,
-		email: student.email,
-		headline: student.headline,
-		token,
-	});
+    const token = await student.generateAuthToken();
+
+    res.status(200).json({
+        name: student.name,
+        email: student.email,
+        headline: student.headline,
+        token,
+    });
 });
 
 //@desc Get Student Profile
 //@route /profile/me
 //@access private
 const getStudentProfile = asyncHandler(async (req, res) => {
-	const student = await Student.findById(req.student._id);
+    const student = await Student.findById(req.student._id);
 
-	if (!student) {
-		res.status(404);
-		throw new Error("Can't get student profile");
-	}
+    if (!student) {
+        res.status(404);
+        throw new Error("Can't get student profile");
+    }
 
-	res.json({
-		_id: student._id,
-		name: student.name,
-		email: student.email,
-		headline: student.headline,
-		isInstructor: student.isInstructor,
-	});
+    res.json({
+        student,
+    });
 });
 
 //@desc Update Student Profile
 //@route /profile/me
 //@access private
 const updateStudentProfile = asyncHandler(async (req, res) => {
-	const student = await Student.findById(req.student._id);
+    const student = await Student.findById(req.student._id);
 
-	const { name, email, headline } = req.body;
+    const { name, email, headline } = req.body;
 
-	if (student) {
-		student.name = name || student.name;
-		student.email = email || student.email;
-		student.headline = headline || student.headline;
+    if (student) {
+        student.name = name || student.name;
+        student.email = email || student.email;
+        student.headline = headline || student.headline;
 
-		if (req.body.password) {
-			student.password = req.body.password;
-		}
+        if (req.body.password) {
+            student.password = req.body.password;
+        }
 
-		await student.save();
-		res.status(200).json({
-			_id: student._id,
-			name: student.name,
-			email: student.email,
-		});
-	} else {
-		res.status(404);
-		throw new Error("Student not found");
-	}
+        await student.save();
+        res.status(200).json({
+            _id: student._id,
+            name: student.name,
+            email: student.email,
+        });
+    } else {
+        res.status(404);
+        throw new Error("Student not found");
+    }
 });
 
 //@desc Update Student Image
 //@route /profile/me
 //@access private
 const updateStudentImg = asyncHandler(async (req, res) => {
-	if (!req.body.image) {
-		res.status(404);
-		throw new Error("Fields is required");
-	}
+    if (!req.body.image) {
+        res.status(404);
+        throw new Error("Fields is required");
+    }
 
-	let uploadRes;
-	try {
-		uploadRes = await cloudinary.v2.uploader.upload(req.body.image, {
-			folder: "image",
-			resource_type: "auto",
-		});
-	} catch (error) {
-		return res.status(400).json({ message: error });
-	}
+    let uploadRes;
+    try {
+        uploadRes = await cloudinary.v2.uploader.upload(req.body.image, {
+            folder: "image",
+            resource_type: "auto",
+        });
+    } catch (error) {
+        return res.status(400).json({ message: error });
+    }
 
-	const student = await Student.findByIdAndUpdate(req.student._id, {
-		$set: {
-			imgProfil: {
-				public_id: uploadRes.public_id,
-				url: uploadRes.secure_url,
-			}
-		}
-	}).catch((error)=>console.log(error, "Studentttt"));
+    const student = await Student.findByIdAndUpdate(req.student._id, {
+        $set: {
+            imgProfil: {
+                public_id: uploadRes.public_id,
+                url: uploadRes.secure_url,
+            },
+        },
+    }).catch((error) => console.log(error, "Studentttt"));
 
-	if(!student){
-		res.status(404);
-		throw new Error("Student not found");
-	}
+    if (!student) {
+        res.status(404);
+        throw new Error("Student not found");
+    }
 
-	res.status(200).json({message: "Success update data"});
+    res.status(200).json({ message: "Success update data" });
 });
 
 // @desc Logout Student
 // @route /logout
 // @access Private
 const logoutStudent = asyncHandler(async (req, res) => {
-	req.student.tokens = req.student.tokens.filter((token) => {
-		return token.token != req.token;
-	});
+    req.student.tokens = req.student.tokens.filter((token) => {
+        return token.token != req.token;
+    });
 
-	await req.student.save();
-	res.status(200).send();
+    await req.student.save();
+    res.status(200).send();
 });
 
 // @desc Logout Student From all devices
 // @route /logout/all
 // @access Private
 const logoutStudentFromAllDevices = async (req, res) => {
-	req.student.tokens = [];
-	await req.student.save();
-	res.status(200).send();
+    req.student.tokens = [];
+    await req.student.save();
+    res.status(200).send();
 };
 
 export {
-	getAllStudentProfile,
-	createStudent,
-	getStudentProfile,
-	updateStudentProfile,
-	loginStudent,
-	logoutStudent,
-	logoutStudentFromAllDevices,
-	createInstructor,
-	updateStudentImg
+    getAllStudentProfile,
+    createStudent,
+    getStudentProfile,
+    updateStudentProfile,
+    loginStudent,
+    logoutStudent,
+    logoutStudentFromAllDevices,
+    createInstructor,
+    updateStudentImg,
 };
--- src/models/Course.js
+++ src/models/Course.js
@@ -3,90 +3,95 @@ import courseReviewSchema from "./CourseReview.js";
 
 const { Schema } = mongoose;
 
-const CourseSchema = new Schema({
-	ttl: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	desc: {
-		type: String,
-		required: true,
-	},
-	fullDesc: {
-		type: String,
-		required: true,
-	},
-	courseRequirements: [String],
-	createdBy: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "Instructor",
-		required: true,
-	},
-	img: {
-		public_id: String,
-		url: String,
-	},
-	languageOfCourse: {
-		type: String,
-		default: "english",
-	},
-	enrolled: {
-		type: Number,
-		default: 0,
-	},
-	avgRating: {
-		type: Number,
-		min: 0,
-		max: 5,
-		default: 0,
-	},
-	numOfRatings: {
-		type: Number,
-		default: 0,
-	},
-	category: {
-		type: String,
-		required: true,
-	},
-	badge: {
-		type: String,
-	},
-	prc: {
-		type: Number,
-		required: true,
-	},
-	newPrc: {
-		type: Number,
-		required: true,
-	},
-	videos: [
-		{
-			section: String,
-			sectionDuration: Number,
-			video: [
-				{
-					title: String,
-					url: String,
-					duration: Number
-				}
-			]
-		}
-	],
-	totalDuration: {
-		type: Number,
-		default: 0,
-	},
-	disc: {
-		type: Number,
-		default: 0,
-	},
-	level: {
-		type: String,
-		required: true,
-	},
-	courseReviews: [courseReviewSchema],
-});
+const CourseSchema = new Schema(
+    {
+        ttl: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        desc: {
+            type: String,
+            required: true,
+        },
+        fullDesc: {
+            type: String,
+            required: true,
+        },
+        courseRequirements: [String],
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Instructor",
+            required: true,
+        },
+        img: {
+            public_id: String,
+            url: String,
+        },
+        languageOfCourse: {
+            type: String,
+            default: "english",
+        },
+        enrolled: {
+            type: Number,
+            default: 0,
+        },
+        avgRating: {
+            type: Number,
+            min: 0,
+            max: 5,
+            default: 0,
+        },
+        numOfRatings: {
+            type: Number,
+            default: 0,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+        badge: {
+            type: String,
+        },
+        prc: {
+            type: Number,
+            required: true,
+        },
+        newPrc: {
+            type: Number,
+            required: true,
+        },
+        videos: [
+            {
+                section: String,
+                sectionDuration: Number,
+                video: [
+                    {
+                        title: String,
+                        url: String,
+                        duration: Number,
+                    },
+                ],
+            },
+        ],
+        totalDuration: {
+            type: Number,
+            default: 0,
+        },
+        disc: {
+            type: Number,
+            default: 0,
+        },
+        level: {
+            type: String,
+            required: true,
+        },
+        courseReviews: [courseReviewSchema],
+    },
+    {
+        timestamps: true,
+    }
+);
 
 const Course = mongoose.model("Course", CourseSchema);
 
